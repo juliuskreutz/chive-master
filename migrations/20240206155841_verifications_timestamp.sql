f7e820f452319a32f27b00c2a473473a7f79d63f
Julius Kreutz <julius@kreutz.dev>
2024-02-07 04:00:16 +1100

    migrations are fun

--- migrations/20240206155841_verifications_timestamp.sql
+++ migrations/20240206155841_verifications_timestamp.sql
@@ -1,1 +1,12 @@
-ALTER TABLE verifications ADD timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE verifications RENAME TO verifications_old;
+
+CREATE TABLE IF NOT EXISTS verifications (
+    uid INTEGER PRIMARY KEY NOT NULL,
+    user INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    otp TEXT NOT NULL,
+    timestamp TIMESTAMP
+);
+
+INSERT INTO verifications(uid, user, name, otp, timestamp) SELECT uid, user, name, otp, CURRENT_TIMESTAMP timestamp FROM verifications_old;
+DROP TABLE verifications_old;
