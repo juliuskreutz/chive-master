0bf1d338ac0e917bf06635b283fbb39ec32e6123
Julius Kreutz <julius@kreutz.dev>
2024-07-03 14:44:56 +1000

    more error handling

--- src/stardb.rs
+++ src/stardb.rs
@@ -9,27 +9,17 @@ pub struct ScoreAchievement {
 }
 
 pub async fn get(uid: i64) -> Result<ScoreAchievement> {
-    Ok(
-        if let Ok(score) = reqwest::get(&format!(
-            "http://localhost:8000/api/scores/achievements/{uid}"
-        ))
-        .await?
-        .json::<ScoreAchievement>()
-        .await
-        {
-            score
-        } else {
-            reqwest::Client::new()
-                .put(&format!(
-                    "http://localhost:8000/api/scores/achievements/{uid}"
-                ))
-                .send()
-                .await?
-                .json::<ScoreAchievement>()
-                .await
-                .map_err(|e| anyhow::anyhow!("{e}: {uid}"))?
-        },
-    )
+    if let Ok(response) = reqwest::get(&format!(
+        "http://localhost:8000/api/scores/achievements/{uid}"
+    ))
+    .await
+    {
+        if let Ok(sa) = response.json::<ScoreAchievement>().await {
+            return Ok(sa);
+        }
+    }
+
+    put(uid).await
 }
 
 pub async fn put(uid: i64) -> Result<ScoreAchievement> {
